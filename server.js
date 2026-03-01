f722ed19ca3bef0f04f76d72a834beb6fdbaf2b4
Łukasz Sochacki <sochacki.luki@gmail.com>
2026-03-01 13:07:34 +0100

    Uprade server

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
+let tasks = [];
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
 });
 
+const io = socket(server);
+
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
+});
+
+io.on('connection', (socket) => {
+  socket.emit('updateData', tasks);
+
+  socket.on('addTask', (task) => {
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+
+  socket.on('removeTask', (taskId) => {
+    tasks = tasks.filter((task) => task.id !== taskId);
+    socket.broadcast.emit('removeTask', taskId);
+  });
 });
